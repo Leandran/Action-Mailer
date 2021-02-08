63271af9376a4225f38fb0a2e5a60b496f4ba724
Leandran <leandranm@outlook.com>
2021-02-08 19:39:50 +0200

    updated notification_maile.rb

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,13 @@
+class NotificationMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notification_mailer.new_accout.subject
+  #
+  def new_accout(user)
+    @user = user
+        
+    mail(to: user.email, subject: "Acount #{@user} is now active")
+  end
+end
